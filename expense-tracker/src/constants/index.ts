752bc83fb86f9cdfc782571764debdd606c64a0b
Pratyush2004-oss <pratyush@sitp.ac.in>
2026-07-31 07:22:09 +0530

    Adding backend api url

--- expense-tracker/src/constants/index.ts
+++ expense-tracker/src/constants/index.ts
@@ -1,2 +1,1 @@
-// export const BACKEND_API_URL="http://localhost:5000"
-export const BACKEND_API_URL="https://dv9g3r7v-5000.inc1.devtunnels.ms"
+export const BACKEND_API_URL = "https://expense-tracker-pi-bay-26.vercel.app"
